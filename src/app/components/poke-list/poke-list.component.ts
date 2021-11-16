2a62557b4ac044008104440f001da83d608fdac4
Juan <juandiegopabonrios@gmail.com>
2021-11-16 13:56:50 -0500

    Traer Pokewachos y mostrarlos

--- src/app/components/poke-list/poke-list.component.ts
+++ src/app/components/poke-list/poke-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
@@ -8,6 +10,13 @@ import { PokemonService } from 'src/app/services/pokemon.service';
 })
 export class PokeListComponent implements OnInit {
 
+  displayedColumns: string[] = ["position", "imagen","name"];
+  data: any[] = [];
+  dataSource = new MatTableDataSource<any>(this.data);
+  pokemons = [];
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+  
+
   constructor(private pokeService: PokemonService) { }
 
   ngOnInit(): void {
@@ -15,16 +24,34 @@ export class PokeListComponent implements OnInit {
   }
 
   getPokemons(){
-    for(let i=1; i<= 150; i++){
+    let pokemonDato;
+    for(let i=1; i<= 50; i++){
       this.pokeService.getPokemons(i).subscribe(
         res =>{
-          console.log(res);
+          pokemonDato={
+            position: i,
+            image: res.sprites.front_default,
+            name:res.name
+          };
+          this.data.push(pokemonDato);
+          this.dataSource=new MatTableDataSource(this.data);
+          this.dataSource.paginator =this.paginator;
+          
         },
         err=>{
-          
+          console.log(err);
         }
       );
     }
     
   }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
+
